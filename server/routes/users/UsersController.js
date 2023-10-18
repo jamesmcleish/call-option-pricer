e77c6bb11707c23b28594eb729942e8405afd9c4
james mcleish <jamesmcleish@jamess-MacBook-Pro.local>
2023-10-18 14:42:26 +0100

    added routes, controllers, etc.

--- server/routes/users/UsersController.js
+++ server/routes/users/UsersController.js
@@ -1,5 +1,5 @@
 const db = require('../../db');
-
+const TokenGenerator = require("../../util/token_generator.model");
 
 async function createUser(req, res) {
 
@@ -14,29 +14,44 @@ async function createUser(req, res) {
     console.error('Error creating user:', error);
     res.status(500).json({ error: 'Failed to create user' });
   }
-}
+};
 
-async function getUserById(req, res) {
-  const userId = req.params.id;
+async function tokenGenerator(req,res) {
 
+  const email = req.headers.email;
+  const password = req.headers.password;
+  const query = 'SELECT * FROM users WHERE email = $1';
   try {
-    const query = 'SELECT * FROM users WHERE id = $1';
-    const values = [userId];
-
-    const result = await pool.query(query, values);
+    const result = await db.query(query, [email]);
 
     if (result.rows.length === 0) {
-      res.status(404).json({ error: 'User not found' });
+      console.log("Auth error: User not found");
+      res.status(401).json({ message: "Authentication error: User not found" });
     } else {
-      res.json(result.rows[0]);
+      const user = result.rows[0];
+      
+      if (user.password !== password) {
+        console.log("Auth error: Passwords do not match");
+        res.status(401).json({ message: "Authentication error: Passwords do not match" });
+      } else {
+        const token = TokenGenerator.jsonwebtoken(email); 
+        res.status(201).json({ token, message: "OK"});
+      }
     }
   } catch (error) {
-    console.error('Error getting user:', error);
-    res.status(500).json({ error: 'Failed to get user' });
+    console.error("Error:", error);
+    res.status(500).json({ message: "Internal server error" });
   }
 }
 
+async function saveProject(req,res) {
+  
+}
+
+
+
 module.exports = {
   createUser,
-  getUserById,
+  tokenGenerator,
+  saveProject,
 };
---
+++ toDo.md
@@ -0,0 +1,5 @@
+Protect projects page with token... absence of token leads to login page
+Write tests
+create save project route and display black and scholes calculation in table below
+create delete project route and place button next to each table row
+write api call to get risk free rate
